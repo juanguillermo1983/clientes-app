c11986e3f389ee5f06ed5036145a5b622c725495
juanguillermo1983 <juanguillermo83@outlook.com>
2023-02-20 23:11:44 -0300

    Etapa 1 Angular, datos estaticos ok

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'juanito', apellido: 'alvarez', email: 'correo1@gmail.com', createAt: '2023-02-20'},
+    {id: 2, nombre: 'marco', apellido: 'alvarez', email: 'correo2@gmail.com', createAt: '2023-02-20'},
+    {id: 3, nombre: 'pedro', apellido: 'perez', email: 'correo3@gmail.com', createAt: '2023-02-20'},
+    {id: 4, nombre: 'arndres', apellido: 'vargas', email: 'correo4@gmail.com', createAt: '2023-02-20'},
+    {id: 5, nombre: 'carlos', apellido: 'silva', email: 'correo5@gmail.com', createAt: '2023-02-20'},
+    {id: 6, nombre: 'renato', apellido: 'Álvarez', email: 'correo6@gmail.com', createAt: '2023-02-20'}
+  ];
